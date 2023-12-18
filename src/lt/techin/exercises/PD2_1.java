6386af98513fd227edd1a474ae97575ea8bf460e
Tomas <151562335+TomSabMGTfan@users.noreply.github.com>
2023-12-18 20:19:56 +0200

    PD2_1

---
+++ src/lt/techin/exercises/PD2_1.java
@@ -0,0 +1,11 @@
+package lt.techin.exercises;
+
+public class PD2_1 {
+    public static void main (String[] args) {
+        System.out.println("Welcome to Java");
+        System.out.println("Learning Java Now");
+        System.out.println("Programing is fun");
+    }
+
+
+}
