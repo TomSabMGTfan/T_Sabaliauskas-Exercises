f0165b2e1be967903a33645dc3413bb5db2b7c6c
Tomas <151562335+TomSabMGTfan@users.noreply.github.com>
2023-12-28 21:43:10 +0200

    PD2_4 padarytas

---
+++ PD2/src/lt/techin/PD2_4.java
@@ -0,0 +1,14 @@
+package lt.techin;
+
+import java.util.Scanner;
+
+public class PD2_4 {
+    public static void main(String[] args) {
+        System.out.println("Sveiki, iveskite savo varda:");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.println("Sveiki kolega, " + name);
+        System.out.println("Kaip nuotaikos, kaip siaip viskas einasi? ");
+
+    }
+}
