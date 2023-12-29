880accf3220d0416713781cd4f2dabae9f9dfeb2
Tomas <151562335+TomSabMGTfan@users.noreply.github.com>
2023-12-29 10:38:00 +0200

    PD2_5 atlikta

---
+++ PD2/src/lt/techin/PD2_5.java
@@ -0,0 +1,22 @@
+package lt.techin;
+
+public class PD2_5 {
+    public static void main(String[] args) {
+        int t1 = 3;
+        int t2 = 10;
+        /*int t3 = 8;
+        int t4 = 1;*/
+
+        int result1 = t1 + t2;
+        System.out.println(result1);
+        int result2 = t1 * t2;
+        System.out.println(result2);
+        int result3 = result2 / result1;
+        System.out.println(result3);
+
+//        int result4 = result3 + result2 + result1;
+//        System.out.println(result4);
+
+
+    }
+}
