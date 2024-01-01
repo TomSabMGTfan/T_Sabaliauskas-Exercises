b3a9e5ab56a93f76b267ce04c900c9bfe0b13e7a
Tomas <151562335+TomSabMGTfan@users.noreply.github.com>
2024-01-01 19:59:23 +0200

    PD3_3 Atliktas

---
+++ PD3/src/lt/techin/PD3_3.java
@@ -0,0 +1,38 @@
+package lt.techin;
+
+public class PD3_3 {
+    public static void main(String[] args) {
+        //Parašykite programą, kuri demonstruoja float aritmetikos specifiką (pvz., apvalinimo klaidas),
+        //naudodami float ir double kintamuosius
+
+        float fnum1 = 0.1f;
+        float fnum2 = 0.1f;
+        float fnum3 = 0.1f;
+        float fnum4 = 0.1f;
+        float fnum5 = 0.1f;
+        float fnum6 = 0.1f;
+        float fnum7 = 0.1f;
+        float fnum8 = 0.1f;
+        float fnum9 = 0.1f;
+        float fsum1 = fnum1 + fnum2 + fnum3 + fnum4 + fnum5 + fnum6 + fnum7 + fnum8 + fnum9;
+        System.out.println("float: " + fsum1);
+
+        double dnum1 = 0.1d;
+        double dnum2 = 0.1d;
+        double dnum3 = 0.1d;
+        double dnum4 = 0.1d;
+        double dnum5 = 0.1d;
+        double dnum6 = 0.1d;
+        double dnum7 = 0.1d;
+        double dnum8 = 0.1d;
+        double dnum9 = 0.1d;
+        double dsum1 = dnum1 + dnum2 + dnum3 + dnum4 + dnum5 + dnum6 + dnum7 + dnum8 + dnum9;
+        System.out.println("double: " + dsum1);
+
+
+
+
+
+
+    }
+}
