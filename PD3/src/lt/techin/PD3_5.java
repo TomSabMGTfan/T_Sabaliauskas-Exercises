39538db447b63cbcef3c3e4d689c93c552658046
Tomas <151562335+TomSabMGTfan@users.noreply.github.com>
2024-01-01 20:53:02 +0200

    PD3_5 atlikti pakeitimai

--- PD3/src/lt/techin/PD3_5.java
+++ PD3/src/lt/techin/PD3_5.java
@@ -4,17 +4,17 @@ public class PD3_5 {
     public static void main(String[] args) {
         //Parašykite programą, kuri demonstruoja skirtingų primitivių duomenų tipų konvertavimą (casting),
         // pvz., int į double, long į int, ir pan.
-        int intNumber1 = 127;
-        double doubleNumber = intNumber1;
-        System.out.println("127 --> " + doubleNumber);
+        int intNum1 = 127;
+        double doubleNum = intNum1;
+        System.out.println("127 --> " + doubleNum);
 
-        long longNumber1 = 15000000000L;
-        int intNumber2 = (int) longNumber1;
-        System.out.println(longNumber1);
+        long longNum1 = 15000000000L;
+        int intNum2 = (int) longNum1;
+        System.out.println(longNum1);
 
-        byte byteNumber = 127;
-        short shortNumber = byteNumber;
-        System.out.println(shortNumber);
+        byte byteNum = 127;
+        short shortNum = byteNum;
+        System.out.println(shortNum);
 
 
 
