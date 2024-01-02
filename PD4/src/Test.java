208fd3db05b1a7010e5ff0f0994f90a057bebd5b
Tomas <151562335+TomSabMGTfan@users.noreply.github.com>
2024-01-02 22:21:35 +0200

    Test

---
+++ PD4/src/Test.java
@@ -0,0 +1,14 @@
+public class Test {
+
+        String fname = "John";
+        String lname = "Doe";
+        int age = 24;
+
+        public static void main(String[] args) {
+            Test myObj = new Test();
+            System.out.println("Name: " + myObj.fname + " " + myObj.lname);
+            System.out.println("Age: " + myObj.age);
+        }
+    }
+
+
