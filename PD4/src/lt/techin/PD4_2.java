4da262dadb40db9f169c13c093c833d672716e59
Tomas <151562335+TomSabMGTfan@users.noreply.github.com>
2024-01-03 20:28:02 +0200

    PD4_2 padarytas

---
+++ PD4/src/lt/techin/PD4_2.java
@@ -0,0 +1,25 @@
+package lt.techin;
+
+public class PD4_2 {
+    public static void main(String[] args) {
+        //Parašykite programą, kuri lygina du skaičius naudodama visus palyginimo operatorius (==, !=, >,
+        //<, >=, <=) ir atspausdina kiekvieno palyginimo rezultatą.
+
+        int x = 3;
+        int y = 5;
+        System.out.println("x= " + x);
+        System.out.println("y= " + y);
+
+        System.out.println("x==y: " + (x==y));
+        System.out.println("x!=y: " + (x!=y));
+        System.out.println("x>y: " + (x>y));
+        System.out.println("x<y: " + (x<y));
+        System.out.println("x>=y: " + (x>=y));
+        System.out.println("x<=y: " + (x<=y));
+
+
+
+
+
+    }
+}
