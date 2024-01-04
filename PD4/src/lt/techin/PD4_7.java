bfbd7404916f6968ea0e66e37ffa546785579231
Tomas <151562335+TomSabMGTfan@users.noreply.github.com>
2024-01-04 21:21:30 +0200

    PD4_7 padarytas (?)

---
+++ PD4/src/lt/techin/PD4_7.java
@@ -0,0 +1,9 @@
+package lt.techin;
+
+public class PD4_7 {
+    public static void main(String[] args) {
+//Parašykite programą, kuri parodo Java operatorių eiliškumą.
+    int result = 5 + 3 / 2 - 3 * 9 % 2;
+        System.out.println(result);
+    }
+}
