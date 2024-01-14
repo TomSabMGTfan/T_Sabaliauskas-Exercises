5621acec0b6b52ef70f9fca4774e9cda413a40c5
Tomas <151562335+TomSabMGTfan@users.noreply.github.com>
2024-01-14 16:56:26 +0200

    PD5_2

---
+++ PD5/src/lt/techin/PD5_2.java
@@ -0,0 +1,21 @@
+package lt.techin;
+
+public class PD5_2 {
+    public static void main(String[] args) {
+        //Parašykite programą, kuri atspausdintų daugybos lentelę nuo 1 iki 10.
+
+        int end = 10;
+
+        for (int start = 1; start <= end; start ++) {
+            int rez1 = start * 1;
+            System.out.println("1" + " " + "x" + " "+start+  " " + "="+ " " + rez1);
+        }
+        System.out.println("***************");
+
+
+
+
+
+
+    }
+}
