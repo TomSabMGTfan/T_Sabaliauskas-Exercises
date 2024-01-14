511d3498d58c1d19bf180f4b679e80fa2452bab6
Tomas <151562335+TomSabMGTfan@users.noreply.github.com>
2024-01-14 19:19:06 +0200

    PD6_1.java

--- PD6/src/lt/techin/PD6_1.java
+++ PD6/src/lt/techin/PD6_1.java
@@ -1,12 +1,20 @@
 package lt.techin;
 
+import java.util.Arrays;
+
 public class PD6_1 {
 
     public static void main(String[] args) {
 
         //Parašykite programą, kuri kopijuoja masyvą.
 
-        
+        int[] originalArray = {1, 2, 3, 4, 5, -10}; //masyvas
+        int[] clonedArray = new int[originalArray.length]; //kuriam nauja dezute su tokiu maciu masyvo dydziu
+        for (int i = 0; i <originalArray.length; i++){
+            clonedArray[i] = originalArray[i]; //int i=0 kopijuojame nuo pradzios, is vienos dezutes paimam ideda
+        }
+        System.out.println(Arrays.toString(originalArray));
+        System.out.println(Arrays.toString(clonedArray));
 
     }
 }
