1c319c699e470ec26c3cb1f827277e467b9d4c63
Tomas <151562335+TomSabMGTfan@users.noreply.github.com>
2024-01-14 17:39:35 +0200

    PD7

--- PD7/src/lt/techin/tomas/PD7_1.java
+++ PD7/src/lt/techin/tomas/PD7_1.java
@@ -1,7 +1,17 @@
+/**
+ * Parašykite Java metodą, kuris grąžina skaičiaus kvadratą.
+ */
+
 package lt.techin.tomas;
 
 public class PD7_1 {
     public static void main(String[] args) {
+        System.out.println(getNumPow(10));
+    }
 
+    //public static int getNumPow(int number){
+    public static double getNumPow(int number){
+        //return number * number;
+        return Math.pow(number, 2.0);
     }
 }
